d1799d8c48fc5d442395387787563abbdd029fb3
madi <kulomady@gmail.com>
2019-06-30 15:58:48 +0700

    add basic ui classification

--- app/src/main/java/com/mine/mytensorflow/ClassificationFragment.kt
+++ app/src/main/java/com/mine/mytensorflow/ClassificationFragment.kt
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_classification.*
 
 class ClassificationFragment : Fragment() {
 
@@ -15,6 +18,19 @@ class ClassificationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_classification, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        options.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+
+            override fun onItemSelected(parent:AdapterView<*>, view: View, position: Int, id: Long){
+                Toast.makeText(context, "" + options.selectedItem ,Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>){
+            }
+        }
     }
 
 
